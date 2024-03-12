4ed98218b5adfb6e9f1c9995cb8dfd3390c6ceb0
Harsika Kumari <kumariharsika8@gmail.com>
2024-03-12 22:13:25 +0530

    creating component

---
+++ heading.jsx
@@ -0,0 +1,13 @@
+const react = require("react");
+
+function Heading() {
+    return <div> <h1>REACT is FUN</h1> <p>
+    React is a free and open-source front-end JavaScript library for building
+    user interfaces based on components. It is maintained by Meta and a
+    community of individual developers and companies. React can be used to
+    develop single-page, mobile, or server-rendered applications with
+    frameworks like Next.js.
+  </p>
+  </div>
+}
+export default Heading;
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const react = require("react");
-const reactDom = require("react-dom")
+const reactDom = require("react-dom");
+import Heading from "./heading.jsx"
+
+react.render(
+  <div>
+    <Heading/>
+  </div>,
+  document.getElementById("root")
+);
